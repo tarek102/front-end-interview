0368bf56b0afdf4cb9a760c06cf7fca7e3973aca
unknown <tarekyosry8@gmail.com>
2023-06-29 11:31:20 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Header } from './components/Header';
+import { Form } from './components/Form';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <Form />
     </div>
   );
 }
